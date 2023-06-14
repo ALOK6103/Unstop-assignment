6ad0eaf795e3c1639814b7add68a5b7d78ab693b
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-14 20:20:32 +0530

    updated backend

--- backend/routes/seat.routes.js
+++ backend/routes/seat.routes.js
@@ -34,23 +34,70 @@ seatRouter.post("/reserve",async(req,res)=>{
       return res.status(400).json({ error: 'Maximum 7 seats can be booked at a time' });
     }
 
+    if (numSeats <= 0) {
+      return res.status(400).json({ error: 'Number of seats must be greater than 0' });
+    }
+
+
+
     // Find available seats in one row
-    const availableSeatsInOneRow = await seatModel.find({
-      isBooked: false,
-      seatNumber: { $lt: 8 },
-    })
-      .sort({ row: 1, seatNumber: 1 })
-      .limit(numSeats);
+    const availableSeatsInOneRow = await seatModel.aggregate([
+      { $match: { isBooked: false } },
+      { $group: { _id: "$row", count: { $sum: 1 } } },
+      { $match: { count: { $gte: numSeats } } },
+      { $sort: { _id: 1 } },
+      { $lookup: { from: "seats", localField: "_id", foreignField: "row", as: "seats" } },
+      { $unwind: "$seats" },
+      { $match: { "seats.isBooked": false } },
+      { $sort: { "seats.seatNumber": 1 } },
+      { $limit: numSeats }
+    ]);
 
     if (availableSeatsInOneRow.length >= numSeats) {
       // Reserve seats in one row
       let temp=[]
-      availableSeatsInOneRow.forEach(async (seat) => {
-        seat.isBooked = true;
-        temp.push(seat.seatNumber)
-        await seat.save();
+      const seatIds = availableSeatsInOneRow.map(seat => seat.seats._id);
+      await seatModel.updateMany({ _id: { $in: seatIds } }, { $set: { isBooked: true } });
+      availableSeatsInOneRow.map((seat)=>{
+        temp.push(seat.seats.seatNumber)
       });
-      return res.json({ message: temp });
+      // return res.json({ message: 'Seats reserved successfully' });
+      return res.json({ message: temp});
+    }
+
+    // Reserve seats in nearby rows
+    const lastReservedRow = await seatModel.findOne({ isBooked: true }).sort({ row: -1 });
+    const nextRow = lastReservedRow ? lastReservedRow.row + 1 : 1;
+
+    const availableSeatsNearby = await seatModel.aggregate([
+      { $match: { isBooked: false, row: nextRow } },
+      { $sort: { seatNumber: 1 } },
+      { $limit: numSeats }
+    ]);
+
+    if (availableSeatsNearby.length >= numSeats) {
+      // Reserve seats in nearby rows
+      let temp=[]
+      const seatIds = availableSeatsNearby.map(seat => seat._id);
+      await seatModel.updateMany({ _id: { $in: seatIds } }, { $set: { isBooked: true } });
+      awail
+      availableSeatsInOneRow.map((seat)=>{
+        temp.push(seat.seats.seatNumber)
+      });
+      // return res.json({ message: 'Seats reserved successfully' });
+      return res.json({ message: temp});
+    }
+
+    if (availableSeatsInOneRow.length >= numSeats) {
+      // Reserve seats in one row
+      let temp=[]
+      const seatIds = availableSeatsInOneRow.map(seat => seat.seats._id);
+      await seatModel.updateMany({ _id: { $in: seatIds } }, { $set: { isBooked: true } });
+      availableSeatsInOneRow.map((seat)=>{
+        temp.push(seat.seats.seatNumber)
+      });
+      // return res.json({ message: 'Seats reserved successfully' });
+      return res.json({ message: temp});
       // return res.json({ message: 'Seats reserved successfully' });
     } else {
       // Find available seats in nearby rows
